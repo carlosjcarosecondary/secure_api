87908af6db83fa3f21f4f86afbec20d852401126
Carlos Caro <carlosjesuscaro@icloud.com>
2018-03-11 01:34:44 -0500

    Bagel project

--- bagel/models.py
+++ bagel/models.py
@@ -15,7 +15,7 @@ class User(Base):
 	def hash_password(self, password):
 		self.password_hash = pwd_context.encrypt(password)
 
-	def verify_password(self, password)
+	def verify_password(self, password):
 		return pwd_context.verify(password, self.password_hash)
 
 class Bagel(Base):
